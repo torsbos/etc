3abfd5f5e295861b9ea8c19f13c0c9c4cc73b42f
t <252655163+torsbos@users.noreply.github.com>
2026-03-15 23:16:09 +0100

    delete stuff

--- randbg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# a simple random background picker using ls, shuf and feh
-# change the dir to match the folder you want
-# GPLv3 license
-
-img=$(ls ~/pic/bg | shuf -n 1)
-feh --no-fehbg --bg-fill ~/pic/bg/$img
--- scr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# screengrab with import with automatic naming
-# ex 260219_11-28-06.jpg
-# GPLv3 license
-
-date=$(date "+%y%m%d_%H-%M-%S")
-import $date.jpg
