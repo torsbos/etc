71ad684a435cc4caef26ec95f8d37e27777ab2c4
t <252655163+torsbos@users.noreply.github.com>
2026-03-18 10:37:33 +0100

    add date and bat status

--- sf.sh
+++ sf.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 cat << EOF
-.  ) )
-.  ( (    $(date '+%T') 
-. |~~~|]  $(uptime -p)
-. \___/   bat: $(cat /sys/class/power_supply/BAT0/capacity)%
+.
+.  ) )    $(date '+%T (%d/%m)') 
+.  ( (
+. |~~~|]  up   $(uptime -p | sed s.up\ ..)
+. \___/   bat  $(cat /sys/class/power_supply/BAT0/capacity)% ($(cat /sys/class/power_supply/BAT0/status))
+.
 EOF
