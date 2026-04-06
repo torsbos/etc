162490cd0e0524fac6de22cfcd8350f4e0492c5c
t <252655163+torsbos@users.noreply.github.com>
2026-04-06 16:30:47 +0200

    ny kurs

--- cup.sh
+++ cup.sh
@@ -6,7 +6,7 @@
 # GPLv3 license
 
 curl -s \
-'https://cloud.timeedit.net/gu/web/schema/ri6Q58224n7055QQ13Z67Y6Z0ZyQ3106.ics' \
+'https://cloud.timeedit.net/gu/web/schema/ri6Q58225n7055QQ14Z60Y6Z0ZyQ6107.ics' \
 > ~/calcurse-import
 calcurse -i ~/calcurse-import
 rm ~/calcurse-import
